3e8c373b7d2ac7c8495b916efbc5086853bbc463
kdima90 <kdima19901990@gmail.com>
2024-02-16 21:11:53 +0100

    init apigateway project for open school

---
+++ src/main/java/kdima90/openschoolapigateway/OpenschoolApigatewayApplication.java
@@ -0,0 +1,13 @@
+package kdima90.openschoolapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenschoolApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenschoolApigatewayApplication.class, args);
+	}
+
+}
